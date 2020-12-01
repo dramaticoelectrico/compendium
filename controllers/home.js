b0d617921a097068b578d553a866a226666af53d
james mcmenemy <cltc1888@yahoo.com>
2020-11-30 18:44:01 -0600

    compendium: add tags to home page

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,11 @@
 const Gallery = require('../models/gallery')
+const Tags = require('../models/tag')
 const Utils = require('../utils/templateUtils')
 const Styles = require('../utils/globalStyles')
 exports.getHomePage = async (req, res, next) => {
   try {
     const gallery = await Gallery.find().sort({ _id: -1 })
+    const tags = await Tags.find()
     const user =
       req.headers.cookie && decodeURI(req.headers.cookie.split('name=')[1])
     res.render('home', {
@@ -12,6 +14,7 @@ exports.getHomePage = async (req, res, next) => {
       templateFn: Utils.getImages,
       gallery,
       user,
+      tags,
     })
   } catch (error) {
     next(error)
