71d4b303f9b45263d45fefe0b59cd1c41a553d31
james mcmenemy <cltc1888@yahoo.com>
2021-01-06 20:22:11 -0800

    compendium: add tags to home page

--- controllers/home.js
+++ controllers/home.js
@@ -2,13 +2,20 @@ const Gallery = require('../models/gallery')
 const Tags = require('../models/tag')
 const Utils = require('../utils/templateUtils')
 const Styles = require('../utils/globalStyles')
+const User = require('../models/user')
 exports.getHomePage = async (req, res, next) => {
   try {
     const gallery = await Gallery.find().sort({ _id: -1 })
     const tags = await Tags.find()
     const user =
       req.headers.cookie &&
-      decodeURI(req.headers.cookie.split(';')[0].split('=')[1])
+      decodeURI(
+        req.headers.cookie
+          .split(';')
+          .filter((item) => item.includes('name='))[0]
+          .split('=')[1]
+      )
+
     res.render('home', {
       title: 'Compendium Pendulum',
       css: Styles.globalCSS,
